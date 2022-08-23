631cb765027f9d222518892c37ddad6fadddc5b8
fabioemoutinho <fabioemoutinho@gmail.com>
2022-08-23 16:36:40 -0300

    Step 8: Authentication State P2 Solution

--- src/app/store/auth/auth.reducer.ts
+++ src/app/store/auth/auth.reducer.ts
@@ -4,11 +4,15 @@ import * as AuthActions from './auth.actions';
 export const authFeatureKey = 'auth';
 
 export interface State {
-
+  userId: string | null;
+  username: string | null;
+  token: string | null;
 }
 
 export const initialState: State = {
-
+  userId: null,
+  username: null,
+  token: null,
 };
 
 export const reducer = createReducer(
