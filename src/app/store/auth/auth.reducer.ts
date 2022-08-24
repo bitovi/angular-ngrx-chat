317c342cbd777845bcf8d76a3cb34cc1bcf2061c
fabioemoutinho <fabioemoutinho@gmail.com>
2022-08-24 10:55:57 -0300

    Step 9: Authentication State P3 Solution

--- src/app/store/auth/auth.reducer.ts
+++ src/app/store/auth/auth.reducer.ts
@@ -18,8 +18,11 @@ export const initialState: State = {
 export const reducer = createReducer(
   initialState,
 
-  on(AuthActions.loadAuths, state => state),
-  on(AuthActions.loadAuthsSuccess, (state, action) => state),
-  on(AuthActions.loadAuthsFailure, (state, action) => state),
-
+  on(AuthActions.loginSuccess, (state, { userId, username, token }) => ({
+    ...state,
+    userId,
+    username,
+    token,
+  })),
+  on(AuthActions.logout, () => initialState)
 );
