a67db69f7e8b9dccf23836757f6694dfc78f8051
Wade H <wade@bitovi.com>
2022-11-23 11:20:38 -0500

    Add user info to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { logout } from '../store/login/login.actions';
+import { selectUserId, selectUsername } from '../store/login/login.selectors';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +9,12 @@ import { logout } from '../store/login/login.actions';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  // Select username from store
+  readonly username$ = this.store.select(selectUsername);
+
+  // Select user ID from store
+  readonly userId$ = this.store.select(selectUserId);
+
   constructor(private store: Store) {}
 
   logout(): void {
