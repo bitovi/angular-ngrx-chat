c300e37235c1051971f058e68edcf31866454517
Mark Thompson <mthompson@bitovi.com>
2022-11-29 16:55:45 -0800

    reverted feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
-import { StoreModule } from '@ngrx/store';
-import * as fromLogin from './store/login/login.reducer';
-import { EffectsModule } from '@ngrx/effects';
-import { LoginEffects } from './store/login/login.effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LoginModule, StoreModule.forFeature(fromLogin.loginFeatureKey, fromLogin.reducer), EffectsModule.forFeature([LoginEffects])],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LoginModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/store/login/login.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as fromLogin from './login.reducer';
-
-export const selectLoginState = createFeatureSelector<fromLogin.State>(
-  fromLogin.loginFeatureKey
-);
