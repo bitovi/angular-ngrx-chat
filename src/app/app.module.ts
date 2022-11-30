af75f1d4b49cb1017f82c3531ebb117dbe3526ba
Mark Thompson <mthompson@bitovi.com>
2022-11-29 16:38:09 -0800

    installed ngrx and created login feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromLogin from './store/login/login.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { LoginEffects } from './store/login/login.effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LoginModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LoginModule, StoreModule.forFeature(fromLogin.loginFeatureKey, fromLogin.reducer), EffectsModule.forFeature([LoginEffects])],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
