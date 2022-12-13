8ae44022936af59622266730de3ecfe71d9ab65d
Mark Thompson <mthompson@bitovi.com>
2022-12-12 18:55:50 -0800

    ran ng generate store ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LoginModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LoginModule, StoreModule.forRoot(reducers, { metaReducers }), !environment.production ? StoreDevtoolsModule.instrument() : []],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
